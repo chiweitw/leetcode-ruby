662f39583b129a667f119e074ac51d4f9e7a9f11
Chiwei Huang <cwhuang0523@gmail.com>
2018-10-21 20:53:00 +0800

    submitted 27-Remove-Element

---
+++ 27-Remove-Element.rb
@@ -0,0 +1,20 @@
+# Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+
+# Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+# The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+# @param {Integer[]} nums
+# @param {Integer} val
+# @return {Integer}
+
+def remove_element(nums, val)
+    current_index = 0
+    (0...nums.size).each do |i|
+        if nums[i] != val
+            nums[current_index] = nums[i]
+            current_index += 1
+        end
+    end
+    current_index
+end
