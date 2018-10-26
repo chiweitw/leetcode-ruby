f6f8175cb01096a625ac5cd218416b12c6194264
Chiwei Huang <cwhuang0523@gmail.com>
2018-10-27 00:18:53 +0800

    updated commit - 55.56%

--- 28-Implement-strStr.rb
+++ 28-Implement-strStr.rb
@@ -15,7 +15,7 @@
 # What should we return when needle is an empty string? This is a great question to ask during an interview.
 
 # For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
-
+# Runtime: 52 ms, faster than 55.56% of Ruby online submissions for Implement strStr().
 def str_str(haystack, needle)
     if needle == ""
         0
